f40ef52f457c32f0c87f85ff91b81b13c5cad340
LarsThy <larsgth@stud.ntnu.no>
2016-11-06 20:05:20 +0100

    sync1 (test)

--- Project/js/08donate.js
+++ Project/js/08donate.js
@@ -1,1 +1,23 @@
-donation
+
+
+function donationCounter(donation){{
+	var totalDonations = 0
+	//var donationList = document.createElement("LI");
+	donation = document.getElementById("NewDonation").value;
+	
+	donationList.appendChild(donation);
+	//totalDonations += donation
+	//console.log(totalDonations)
+}
+	var donationList = [1,3, 5, 8]
+
+var totalDonations = donationList.reduce(sum, 0);
+	
+document.getElementById("output").innerHTML = (donationList);
+}
+
+function sum(a, b){
+	return a + b;
+}
+
+console.log(totalDonations);
