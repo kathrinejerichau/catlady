b8d633f1890025440a728092792839bdba3212fa
fredrikif <fiforsa@gmail.com>
2016-11-07 22:15:05 +0100

    00home
    finisha home, endra litt css, kaching.

--- Project/00home.php
+++ Project/00home.php
@@ -19,10 +19,32 @@
 
 	<!-- ***** BODY CONTENT STARTS HERE ***** -->
 	<div id="divBody">
+		<div id="nameOfPage">
+			<h2>Welcome to the Cat Castle</h2>
+		</div>
+		
 		<div id="bodyLeft">
 			<img src="img/homeCat.png" alt="home cat">
 		</div>
-		<div id="bodyRight"></div>
+		
+		<div id="bodyRight">
+			<p>
+				Welcome to Cattius' Cat Castle. The Castle serves as a homeless
+				shelter for cats, where people in the local area can 
+				<a href="03handIn.php">deliver</a> cats they find, or 
+				<a href="01adopt.php">adopt</a> cats from the shelter.
+			</p>
+			<p>
+				The Castle also has an entertainment section, which provides content
+				on a global scale. Feel free to head over to <a href="05fun.php">our
+				entertainment page</a>.
+			</p>
+			<p>
+				Since the Cat Castle is a non-profit organisation, it relies on volunteer
+				work and gifts for funding. If you like the content and services provided
+				by Cattius, please feel free to visit <a href="08donate.php">our donation page</a>.
+			</p>
+		</div>
 	</div>
 	
 	
