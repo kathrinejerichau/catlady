95e1196a7b7b62a2232a0ddb8f22451cd1605d14
LarsThy <larsgth@stud.ntnu.no>
2016-11-08 14:58:28 +0100

    08donate
    forbedret js-koden.
    
    endret styling i html og css med tekst på høyre side og skjema på
    venstre.

--- Project/08donate.php
+++ Project/08donate.php
@@ -24,18 +24,21 @@
 		<div id="nameOfPage">
 			<h2>Donations</h2>
 		</div>
-		<div id="donationForm">
+		
 	
-	
+	<div id="bodyLeft">
 			<p> Want to support us? We are a non-profit organization that needs 
 	funds to help as many cats as possible. All donations go to the well-
 	being of homeless cats. 
 	
 	If you want to remain anonymous you can leave the fields blank. 
 			</p>
+		<br><output id="output"></output>
+	</div>
 	
-	
-	<form onsubmit="event.preventDefault(); donationCounter();" autocomplete="off" 
+	<div id="bodyRight">
+
+	<form id="donationForm" onsubmit="event.preventDefault(); donationCounter();" autocomplete="off" 
 	action="http://folk.ntnu.no/michaedm/norwegian-tax-administration/receive.php" method="post">
 	<input type="text" name="donName" id="donatorName" placeholder="Your name: ">
 	<input type="text" name="donAddress" id="donatorAddress" placeholder="Address: ">
@@ -43,13 +46,13 @@
 	<input type="text" name="donPhone" id="donatorPhone" placeholder="Phone number: ">
 	<input type="number" name="donAmount" id="NewDonation" placeholder="Donation amount....." min="1" >
 	
-	<input type="image" src="img/donationButton.jpg" id="donationButton" value="DONATE NOW" height="75" width="100">
+	<input type="image" src="img/donationButton.jpg" id="donationButton" height="75" width="100" alt="Donate button image">
 	
+	</form>
 
-	<br><output id="output"></output>
 
 	</div>
-
+	</div>
 	
 	
 	<!-- ***** FOOTER CONTENT BELOW ***** -->
--- Project/js/08donate.js
+++ Project/js/08donate.js
@@ -3,14 +3,13 @@
 const donationList = [];
 
 function donationCounter(){
-
+	
 	var donation = document.getElementById("NewDonation").value;
+	if (donation>0) 	{
     var donatedNumber = Number(donation);
   
     donationList.push(donatedNumber);
 
-
-    
 var totalDonations = donationList.reduce(sum, 0);
 
 function sum(a, b){
@@ -21,5 +20,6 @@ function sum(a, b){
   + " contributors has donated to our shelter. Their contributions amounts to a total of "
   + totalDonations + "$ to help local cats.");
 
+	document.getElementById("donationForm").reset();
 }
-
+}
