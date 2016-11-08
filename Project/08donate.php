bf65b4ed60ebb30bf951535cd7e8d1697b22e202
LarsThy <larsgth@stud.ntnu.no>
2016-11-08 14:03:14 +0100

    08donate
    fjernet feil, la inn "nameOfPage", la inn <p> i riktig div for bedre
    estetikk.
    
    css: jobber med styling, ser fortsatt ikke supert ut.

--- Project/08donate.php
+++ Project/08donate.php
@@ -20,19 +20,23 @@
 
 	<!-- ***** BODY CONTENT STARTS HERE ***** -->
 	<div id="divBody">
-		
-		<p> Want to support us? We are a non-profit organization that needs 
+	
+		<div id="nameOfPage">
+			<h2>Donations</h2>
+		</div>
+		<div id="donationForm">
+	
+	
+			<p> Want to support us? We are a non-profit organization that needs 
 	funds to help as many cats as possible. All donations go to the well-
 	being of homeless cats. 
 	
 	If you want to remain anonymous you can leave the fields blank. 
-	</p>
-		
-		
-		<div id="donationForm">
+			</p>
+	
 	
 	<form onsubmit="event.preventDefault(); donationCounter();" autocomplete="off" 
-	action="http://folk.ntnu.no/michaedm/norwegian-tax-administration/receive.php" method="post">>
+	action="http://folk.ntnu.no/michaedm/norwegian-tax-administration/receive.php" method="post">
 	<input type="text" name="donName" id="donatorName" placeholder="Your name: ">
 	<input type="text" name="donAddress" id="donatorAddress" placeholder="Address: ">
 	<input type="email" name="donEmail" id="donatorEmail" placeholder="Email: ">
@@ -41,17 +45,11 @@
 	
 	<input type="image" src="img/donationButton.jpg" id="donationButton" value="DONATE NOW" height="75" width="100">
 	
+
 	<br><output id="output"></output>
 
-	
-	
-	<!-- HVA ER DETTE, FREDRIK? SKILLER FOR SIDENE SOM HAR BILDE PÅ EN HALVDEL OG TEKST PÅ DEN ANDRE?
 	</div>
-		<div id="bodyLeft"></div>
-		<div id="bodyRight"></div>
-	</div>
-	
--->
+
 	
 	
 	<!-- ***** FOOTER CONTENT BELOW ***** -->
