a50f5c93772c674012396f2f010c53cdb8a43479
fiffi95 <sofiajacobsson@live.se>
2016-11-09 14:06:36 +0100

    catCare stuff2

--- Project/04catCare.php
+++ Project/04catCare.php
@@ -24,14 +24,39 @@
 			<h2>Cat care</h2>
 		</div>
 		<div id="bodyLeft">
-		<p>Cats require lots of love from their owner in order to be happy.
+		<img src="img/cat_care.jpg" alt="cat care" width="100%">
+		<img src="img/cat_care_fatCat.jpg" alt="cat care" width="100%">
+		<img src="img/cat_care_cute.jpg" alt="cat care" width="100%">
 
-		Here you can read all about how to take care of your cat.</p>
+		
+		
 			
 		</div>
-		<div id="bodyRight">			
+		<div id="bodyRight">
+			<p>Cat Care Tip:</p>			
 			<canvas id="catCareRight" width="300" height="300"></canvas><br>
-			<img src="img/cat_care.jpg" alt="cat care" width="100%">
+			<p>Cats require lots of love from their owner in order to be happy. Here you can read all about how to take care of your cat.</p>
+
+			<p>
+				Although cats enjoy your company, they also need some privacy. Place the litter box in a quiet spot where the cat can relief itself without interruptions. Avoid heavily scented litters for the litter box. 
+			</p>
+
+			<p>
+				Also, cats like to eat without interruptions. Give your cat time to eat, and cuddle with it afterwards. Don’t overfeed your cat. Obesity if one of the biggest health issues facing cats today.
+			</p>
+
+			<p>
+				The average lifespan of an indoor cat is 13-17 years. Purebred cats are more likely to have genetic medical problems than non-pedigreed cats. Occasionally, a cat with a medical condition might need an adjusted diet. Consult your veterinarian for recommendations. Schedule yearly vet visits to make sure your cat is healthy.
+
+			</p>
+
+			<p>
+				Keep an eye on your cat. Your cat can’t speak english - so you have to be observant of behavioral changes to make sure your cat is healthy.
+			</p>
+
+			<p>
+				Provide your cat with a scratching post. This will keep it from scratching furniture, wood etc.
+			</p>
 		</div>
 	</div>
 	
--- Project/js/04catCare.js
+++ Project/js/04catCare.js
@@ -16,63 +16,37 @@ var rightHeight = bodyRight.offsetHeight;
 const canvasRight = document.getElementById('catCareRight');
 const contextRight = canvasRight.getContext('2d');
 contextRight.canvas.width = 250;
-contextRight.canvas.height = 60;
-contextRight.font = "18px defaulFont";
+contextRight.canvas.height = 100;
+contextRight.font = "18px defaultFont";
 contextRight.fillStyle = "#4e3d42";
 console.log(canvasRight.width);
 console.log(canvasRight.height);
 
-
-
-
-var textArray = ["Cats enjoy a warm cozy place", "Cats are obsessed with catnip ", "raaaawr"];
-
+/* Array which holds the cat care tips. 
+  Each string of text must have a maximum lengh of 25 characters. 
+  This is because of the width of the canvas, and that the tips are supposed to be displayed on one line. 
+*/
+var textArray = ["Cats enjoy a warm cozy place  to rest", "Cats are obsessed with catnip,give them some and they will  go crazy", "Kittens like to play with     things that move. Create your your toy with a string and    some paper"];
 var textIndex = 0;
 
-var imageObj = new Image();
-imageObj.src = "img/catCareTips.jpg";
-
-
-
-
 function swapText() {
-	contextRight.clearRect(0, 0, contextRight.canvas.width, contextRight.canvas.height);
-	//contextRight.drawImage(imageObj, 0, 0, rightWidth, rightWidth * imageObj.height / imageObj.width);
-	//drawBubble(contextRight, 160, 190, 150, 80, 10);
-	drawText("Cat Care Tip:", 20, 20);
-	drawText(textArray[textIndex], 25, 40);
-	textIndex++;
-	if (textIndex > 2) {
-		textIndex = 0;
-	}
+  contextRight.clearRect(0, 0, contextRight.canvas.width, contextRight.canvas.height);
+  lines = textArray[textIndex].match(/.{1,30}/g);
+  contextRight.canvas.height = 25 * lines.length;
+  contextRight.font = "18px defaultFont";
+  for (var i = 0; i < lines.length; i++) {
+    drawText(lines[i], 10, 15+i*25);
+  }
+  textIndex++;
+  if (textIndex > 2) {
+    textIndex = 0;
+  }
 }
 
 /* Draw a text (string) on (x, y) */
 function drawText(text, x, y) {
 	contextRight.fillText(text, x, y);
 }
-
-function drawBubble(ctx, x, y, w, h, radius)
-{
-  var r = x + w;
-  var b = y + h;
-  ctx.beginPath();
-  ctx.strokeStyle="black";
-  ctx.lineWidth="2";
-  ctx.moveTo(x+radius, y);
-  ctx.lineTo(x+radius/2, y-10);
-  ctx.lineTo(x+radius * 2, y);
-  ctx.lineTo(r-radius, y);
-  ctx.quadraticCurveTo(r, y, r, y+radius);
-  ctx.lineTo(r, y+h-radius);
-  ctx.quadraticCurveTo(r, b, r-radius, b);
-  ctx.lineTo(x+radius, b);
-  ctx.quadraticCurveTo(x, b, x, b-radius);
-  ctx.lineTo(x, y+radius);
-  ctx.quadraticCurveTo(x, y, x+radius, y);
-  ctx.stroke();
-}
-
 
 
 drawText("Cat Care Tip:", 20, 20);
