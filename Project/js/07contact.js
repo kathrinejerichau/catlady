58d0bab000c1abe5c9265140bc375def4eec68a5
fredrikif <fiforsa@gmail.com>
2016-11-09 18:00:56 +0100

    contact&donate comments
    #justcommentingthings

--- Project/js/07contact.js
+++ Project/js/07contact.js
@@ -1,8 +1,18 @@
-//javascript for contactForm
+/*
+FILE NAME: 07contact.js
+WRITTEN BY: Fredrik I. Forså
+WHEN: November 2016
+PURPOSE: JavaScript to the contact form.
+		Reset button, and very basic form validator, 
+		making sure the form has content.
+*/
+
+//Function to reset form
 function rsForm(){
 	document.getElementById("contactForm").reset();
 }
 
+//Function to validate form
 function validateForm(){
 	var inputName = document.getElementById("contactName").value;
 	var inputEmail = document.getElementById("contactEmail").value;
