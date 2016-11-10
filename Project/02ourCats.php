e6487b8960a6150f63fa7d4138f5b6d314e01bbf
fredrikif <fiforsa@gmail.com>
2016-11-10 01:37:22 +0100

    ourCats
    Simplified and reduced amount of code needed for slideshow. Added
    comments and validated files.

--- Project/02ourCats.php
+++ Project/02ourCats.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <!--
 FILE NAME: 02ourCats.php
-WRITTEN BY: Lars G. Thyholdt
+WRITTEN BY: Lars G. Thyholdt & Fredrik I. Forså
 WHEN: November 2016
 PURPOSE: Slideshow with cats available for adoption.
 -->
@@ -19,115 +19,21 @@ PURPOSE: Slideshow with cats available for adoption.
 	
 	<!-- ***** BODY CONTENT STARTS HERE ***** -->
 	<div id="divBody">
-	<div id="nameOfPage">
-		<h2>Our Cats</h2>
+		<div id="nameOfPage">
+			<h2>Our Cats</h2>
 		</div>
 	
-	<div id="ourCatsPara">
-	<p> Here you can see pictures of the cats current available for adoption. If you want to 
-	adopt one of these majestic animals, go to our <a href="01adopt.php">adoption page</a>. </p>
+		<div class="catPictures">
+			<p id="catNumber"></p>
+			<img id="catImg" src="img/ourCats/cat00.png" alt="Adorable Anton">
+			<p id="catText"></p>
+		</div>	
 	</div>
-	
-	<div class="catPictures">
-		<div class="numbertextOurCats"> 1/15 </div>
-	  <img src="img/ourCats/cat01.png">
-	  <div class="textOurCats"> Adorable Anthon </div>
-	</div>
-
-	<div class="catPictures">
-	  <div class="numbertextOurCats">2 / 15</div>
-	  <img src="img/ourCats/cat02.png">
-	  <div class="textOurCats">Elegant Eric</div>
-	</div>
-
-	<div class="catPictures">
-	  <div class="numbertextOurCats">3 / 15</div>
-	  <img src="img/ourCats/cat03.png">
-	  <div class="textOurCats">Lazy Larry</div>
-	</div>
-
-	<div class="catPictures">
-	  <div class="numbertextOurCats">4 / 15</div>
-	  <img src="img/ourCats/cat04.png">
-	  <div class="textOurCats">Sleepy Steve</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">5 / 15</div>
-	  <img src="img/ourCats/cat05.png">
-	  <div class="textOurCats">Upperton</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">6 / 15</div>
-	  <img src="img/ourCats/cat06.png">
-	  <div class="textOurCats">Curious Currybomb</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">7 / 15</div>
-	  <img src="img/ourCats/cat07.png">
-	  <div class="textOurCats">Butterbeard</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">8 / 15</div>
-	  <img src="img/ourCats/cat08.png">
-	  <div class="textOurCats">Mr. Whitepaw</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">9 / 15</div>
-	  <img src="img/ourCats/cat09.png">
-	  <div class="textOurCats">Anxious Adam</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">10 / 15</div>
-	  <img src="img/ourCats/cat10.png">
-	  <div class="textOurCats">Bossy Bernt</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">11 / 15</div>
-	  <img src="img/ourCats/cat11.png">
-	  <div class="textOurCats">Dr. Johnson</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">12 / 15</div>
-	  <img src="img/ourCats/cat12.png">
-	  <div class="textOurCats">Sneaking Sherry</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">13 / 15</div>
-	  <img src="img/ourCats/cat13.png">
-	  <div class="textOurCats">Staring Stian</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">14 / 15</div>
-	  <img src="img/ourCats/cat14.png">
-	  <div class="textOurCats">Mushy Martin</div>
-	</div>
-	
-	<div class="catPictures">
-	  <div class="numbertextOurCats">15 / 15</div>
-	  <img src="img/ourCats/cat15.png">
-	  <div class="textOurCats">Fabulous Fredrik</div>
-	</div>
-
-	
-
-	
-	</div>
-	
 	
 	<!-- ***** FOOTER CONTENT BELOW ***** -->
 		<?php include 'footer.php';?>
 </div>
 
-<script src="js/02ourCats.js"> </script>
+<script src="js/02ourCats.js"></script>
 </body>
 </html>
--- Project/js/02ourCats.js
+++ Project/js/02ourCats.js
@@ -1,21 +1,54 @@
-var slideIndex = 0;
+/*
+FILE NAME: 02ourCats.js
+WRITTEN BY: Fredrik I. Forså & Lars G. Thyholdt
+WHEN: November 2016
+PURPOSE: The slideshow for 02ourCats.php.
+*/
 
+//Array and object creation
+var catArray = [];
+var c0 = {catInfo:"Adorable Anton", catSrc:"img/ourCats/cat00.png"};
+var c1 = {catInfo:"Elegant Eric", catSrc:"img/ourCats/cat01.png"};
+var c2 = {catInfo:"Lazy Larry", catSrc:"img/ourCats/cat02.png"};
+var c3 = {catInfo:"Sleepy Steve", catSrc:"img/ourCats/cat03.png"};
+var c4 = {catInfo:"Upperton", catSrc:"img/ourCats/cat04.png"};
+var c5 = {catInfo:"Curious Currybomb", catSrc:"img/ourCats/cat05.png"};
+var c6 = {catInfo:"Butterbread", catSrc:"img/ourCats/cat06.png"};
+var c7 = {catInfo:"Mr Whitepaw", catSrc:"img/ourCats/cat07.png"};
+var c8 = {catInfo:"Anxious Adam", catSrc:"img/ourCats/cat08.png"};
+var c9 = {catInfo:"Bossy Bernt", catSrc:"img/ourCats/cat09.png"};
+var c10 = {catInfo:"Dr. Johnson", catSrc:"img/ourCats/cat10.png"};
+var c11 = {catInfo:"Sneaky Sofia", catSrc:"img/ourCats/cat11.png"};
+var c12 = {catInfo:"Lean Lars", catSrc:"img/ourCats/cat12.png"};
+var c13 = {catInfo:"Mushy Martin", catSrc:"img/ourCats/cat13.png"};
+var c14 = {catInfo:"Fabulous Fredrik", catSrc:"img/ourCats/cat14.png"};
+var c15 = {catInfo:"Kind Kathrine", catSrc:"img/ourCats/cat15.png"};
 
-catSlideshow();
-function catSlideshow() {
+//Pushes objects to array 
+catArray.push(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15);
 
-    var catImg = document.getElementsByClassName("catPictures");
-    for (var i = 0; i < catImg.length; i++) {
-       catImg[i].style.display = "none";
-    }		
-	
-    slideIndex++;
-    if (slideIndex> catImg.length) {
-	slideIndex = 1
-	} 		//resetter slideIndex til 1 når telleren over går forbi catImg.length
+//Counter begins at zero
+var i = 0;
 
-    catImg[slideIndex-1].style.display = "block";
+function catSlideShow(){
+	//Adds values from catArray[i] to various DOMs.
+	document.getElementById("catNumber").innerHTML = i+1 + " / " + catArray.length;
+	document.getElementById("catImg").src = catArray[i].catSrc;
+	document.getElementById("catImg").alt = catArray[i].catInfo;
+	document.getElementById("catText").innerHTML = catArray[i].catInfo;
 
-    setTimeout(catSlideshow, 5000); // setTimeout funker bedre enn setInterval
+		
+	//Resets or increases i (resets if last array entry has been reached)
+	if (i+1 == catArray.length){
+		i = 0;
+	}	
+	else{
+		i++;
+	}
+		
+	//Timeout sets ms delay before function runs again
+	setTimeout(catSlideShow, 3000);
 }
 
+//Calls function above
+catSlideShow();
