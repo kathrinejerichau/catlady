a0329dcd00cc3412ff0b788b9625307ce1d9012a
fredrikif <fiforsa@gmail.com>
2016-11-10 14:20:50 +0100

    07contact validated
    fixed various form-related issues upon validation

--- Project/07contact.php
+++ Project/07contact.php
@@ -45,15 +45,16 @@ PURPOSE: Basic contact info plus form to fill out.
 			<input type="text" name="contactName" id="contactName" placeholder="You name: " required>
 			<input type="email" name="contactEmail" id="contactEmail" placeholder="Your email" required>
 			<select id="about" name="about" size="1" required>
+				<option value="">Choose an option</option>
 				<option value="adopt">I want to adopt a cat</option>
 				<option value="deliver">I want to deliver a cat</option>
 				<option value="feedback">I wish to give feedback</option>
 				<option value="donation">I want to donate</option>
 				<option value="other">Other</option>
 			</select>
-			<textarea rows="4" cols="31" form_id="contactForm" name="contactTextBox" id="contactTextBox" placeholder="Your message here:" required></textarea><br>
-			<input type="submit" id="sendButton" onclick="validateForm();" value="Send" height="75" width="100">
-			<input type="button" onclick="rsForm();" id="resetForm" value="Reset" height="75" width="100">
+			<textarea rows="4" cols="31" name="contactTextBox" id="contactTextBox" placeholder="Your message here:" required></textarea><br>
+			<input type="submit" id="sendButton" onclick="validateForm();" value="Send">
+			<input type="button" onclick="rsForm();" id="resetForm" value="Reset">
 			</form>
 		</div>
 	</div>
