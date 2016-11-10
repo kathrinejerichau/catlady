ec1aadff1dcc24a9d36f45a2b08550a1f52f38e2
Kvalheimo <ma_kval@hotmail.com>
2016-11-10 20:23:49 +0100

    Sletting
    Av mellrom

--- Project/05fun.php
+++ Project/05fun.php
@@ -14,67 +14,38 @@
 	<!-- ***** BODY CONTENT STARTS HERE ***** -->
 	<div id="divBody">
 	
-	
 <div id="nameOfPage">
 			<h2>Entertainment </h2>
 		</div>
 		
 <div class="imgContainer">		
   
-  
 	<!-- Upper row of pictures -->
 	
-		<div id="upperRow">
-			
-			
+		<div id="upperRow">		
 				<img class="thumbnail" onclick="onClick(this)" id="myImg1" src="img/fun/funCat1.png" alt="funny cat" >
-		
 				<img class="thumbnail" onclick="onClick(this)" id="myImg2" src="img/fun/funCat2.png" alt="funny cat">
-			
 				<img class="thumbnail" onclick="onClick(this)" id="myImg3" src="img/fun/funCat3.png" alt="funny cat">
-			  
 				<img class="thumbnail" onclick="onClick(this)" id="myImg4" src="img/fun/funCat4.png" alt="funny cat">
-			  
 				<img class="thumbnail" onclick="onClick(this)" id="myImg5" src="img/fun/funCat5.png" alt="funny cat">
-				
-					<!-- Modal -->
-				<div id="funModal" class="modal" onclick="this.style.display='none'">
-				<!-- Close knapp -->
-				<span class="close" onclick="document.getElementById('funModal').style.display='none'">&times;</span>
-				 <!-- Modal Content (The Image) -->
-				 <img class="modal-content" id="modImg">
-			  
-			  </div>
-			  
 		</div>
-			
-			
 	<!-- Lower row of pictures -->
-			
 		<div id="lowerRow">
-			
-			
-				<img class="thumbnail"  onclick="onClick(this)" id="myImg6"  src="img/fun/funCat6.png" alt="funny cat">
-			  
+				<img class="thumbnail"  onclick="onClick(this)" id="myImg6"  src="img/fun/funCat6.png" alt="funny cat">  
 				<img class="thumbnail"  onclick="onClick(this)" id="myImg7"  src="img/fun/funCat7.png" alt="funny cat">
-			  
 				<img class="thumbnail"  onclick="onClick(this)" id="myImg8"  src="img/fun/funCat8.png" alt="funny cat">
-			
 				<img class="thumbnail"  onclick="onClick(this)" id="myImg9"  src="img/fun/funCat9.png" alt="funny cat">
-			  
 				<img class="thumbnail"  onclick="onClick(this)" id="myImg10"  src="img/fun/funCat10.png" alt="funny cat">
-			  
-			
-		
 		</div>
     </div>
+		<!-- Modal -->
+		<div id="funModal" class="modal" onclick="this.style.display='none'">
+		<!-- Close button -->
+		<span class="close" onclick="document.getElementById('funModal').style.display='none'">&times;</span>
+		 <!-- Modal Content (The Image) -->
+		<img class="modal-content" id="modImg">
+		</div>
   </div>
-
-		
-		
-
-	
-	
 	<!-- ***** FOOTER CONTENT BELOW ***** -->
 		<?php include 'footer.php';?>
 </div>
